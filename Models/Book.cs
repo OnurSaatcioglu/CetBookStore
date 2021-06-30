65dc966bda670e52d5d8e89bc829500f3f6604f4
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-06-30 21:19:56 +0300

    Book sınıfı oluşturuldu

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int PageCount { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public decimal Price { get; set; }
+
+
+    }
+}
