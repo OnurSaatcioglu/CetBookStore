24d256d6e7937079b7650ff22062603b6df011ba
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-07-01 18:26:24 +0300

    muhtelif değişiklikler

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CetBookStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,8 @@ namespace CetBookStore.Data
             : base(options)
         {
         }
+
+        public DbSet<Book> Books { get; set; }
+
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,16 +11,22 @@ namespace CetBookStore.Models
 
         public int Id { get; set; }
 
+        [DisplayName("Ad")]
         public string Name { get; set; }
 
+        [DisplayName("Yazar")]
         public string Author { get; set; }
 
+        [DisplayName("Sayfa Sayısı")]
         public int PageCount { get; set; }
 
+        [DisplayName("Yayımlanma Tarihi")]
         public DateTime PublishDate { get; set; }
 
+        [DisplayName("Oluşturulma Tarihi")]
         public DateTime DateCreated { get; set; }
 
+        [DisplayName("Fiyat")]
         public decimal Price { get; set; }
 
 
